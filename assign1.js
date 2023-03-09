8d0e8a3f285fc2f993f480b46c07a99f15ae051a
King's meow <mingee641@gmail.com>
2023-03-09 21:45:59 +0900

    처음이디ㅏㅏ

---
+++ assign1.js
@@ -0,0 +1,41 @@
+
+function getType(data) {
+    if (typeof data === "string") {
+        if(data === "true" || data === "false")
+            return "불리언";
+        else {
+            return "문자열";     
+        }
+    } else if (typeof data === "number") {
+        return "숫자";
+    } else if (Array.isArray(data)) {
+        return "배열";
+    } else if (typeof data === "object") {
+        return "객체";
+    } else if (typeof data === "function") {
+        return "함수";
+    } else {
+        return "알 수 없는 타입";
+    }
+}
+
+function showType() {
+    //input 요소 가져오기
+    var input = document.getElementById("inputText");
+    var data = input.value;
+
+    // 데이터 타입 확인하기 
+    var dataType;
+    
+    if(isNaN(data)) {
+        dataType = getType(data);
+    }
+    else {
+        dataType = "숫자";
+    }
+    
+
+    // 결과 출력하기
+    var result = document.getElementById("result");
+    result.innerHTML = "데이터 타입: " + dataType;
+}
