d6d09b58660544f43f59618ec255612aad1c9e25
sangjung <sangjung245@gmail.com>
2023-07-05 22:15:39 +0900

    최적화

--- week 7/sangjung/js/clock.js
+++ week 7/sangjung/js/clock.js
@@ -1,8 +1,7 @@
 export const initClock = () =>{
     const clock = document.querySelector('.todo-title');
     const startClock = () =>{
-        const now = new Date();
-        clock.textContent = String(now).substring(16,24);
+        clock.textContent = Date().substring(16,24);
     }
     startClock();
     setInterval(startClock, 1000);
